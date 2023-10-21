6150b986876cfaa2045b9148891e700f462b4278
FedasaBote <fedasagete@gmail.com>
2023-10-21 19:44:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/14852491/64910580-cc163700-d70f-11e9-99ec-8c49095a8c3b.png" />
+  <br/>
+  the simplest MERN (MongoDB, Express, React and Node) setup
+</p>
+
+## Getting Started
+
+### Development
+
+1. Install MongoDB and run on default port `27017`
+2. `npm install` in both root directory and `client` directory
+3. `npm start` in both root directory and `client` directory
+4. Head to [localhost:3000](http://localhost:3000) to see the 'My Tasks' app
+
+### Production
+
+```bash
+npm install
+npm run build
+npm run start:prod
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/simple-mern");
+const app = express();
+
+const routeTasks = require("./src/routes/tasks");
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(bodyParser.json());
+
+app.use("/api/tasks", routeTasks, (req, res) => res.sendStatus(401));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
